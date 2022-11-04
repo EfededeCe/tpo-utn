2d4578b96679985da5fc69c16a1138cce725733c
Fede dC <fede_delcorro@hotmail.com>
2022-11-04 12:18:11 -0300

    tp v-4

---
+++ main.js
@@ -0,0 +1,49 @@
+/*  Validacion de formulario y proceso:
+
+Voy a presupuestar un catering segun la cantidad de personas
+y el tipo de comida.
+
+Seleccionar primero cuantas personas:
+
+hasta 20: 1000,1500,2500 por persona
+hasta 50: un 10% menos
+hasta 100: un 20% menos
+
+Seleccionar bajilla:
+Si: $400 por persona
+
+Seleccionar mobiliario (mesas, sillas, manteles)
+Si: $200 por persona
+
+Seleccionar decoración
+Flores: $150 por persona
+Globos: $50 por persona
+Especial Cumpleaños: $300 por persona
+Especial Casamiento: $500 por persona
+Especial Casamiento 2: $800 por persona
+
+Hacer la cuenta segun la cantidad de personas:
+$ en un solo pago 50% antes, 50% al finalizar el evento
+
+Ofrecer (previo un pago del 30%) pagar en 6 o 12 cuotas el monto restante:
+6 cuotas con una taza del 4% mensual
+12 cuotas con una taza del 8% mensual
+
+*/
+
+let nombre = document.querySelector("#fullName").value; 
+let mail = document.querySelector("#email").value; 
+let tel = document.querySelector("#tel").value;
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    console.log($("#fullName").value)
+    });
+
